97f295acf376b3051aa72e860e305b3f4386e2f7
DC339 <2164485200@qq.com>
2019-05-04 16:23:30 +0800

    news auto updata

---
+++ ajax自动更新/getNews.php
@@ -0,0 +1,18 @@
+<?php
+header('content-type:text/html,charset="utf-8"');
+error_reporting(0);
+
+$arr = array(
+	array('title'=>'中日驻英外交官撰文互称对方国家为"伏地魔"','date'=>'2014-1-6'),
+	array('title'=>'德国女总理默克尔滑雪时摔倒 骨盆断裂','date'=>'2014-1-6'),
+	array('title'=>'德国女总理默克尔滑雪时摔倒 骨盆断裂','date'=>'2014-1-6'),
+	array('title'=>'中日驻英外交官撰文互称对方国家为"伏地魔"','date'=>'2014-1-6'),
+	array('title'=>'德国女总理默克尔滑雪时摔倒 骨盆断裂','date'=>'2014-1-6'),
+	array('title'=>'德国女总理默克尔滑雪时摔倒 骨盆断裂','date'=>'2014-1-6'),
+	array('title'=>'中日驻英外交官撰文互称对方国家为"伏地魔"','date'=>'2014-1-6'),
+	array('title'=>'德国女总理默克尔滑雪时摔倒 骨盆断裂','date'=>'2014-1-6'),
+	array('title'=>'德国女总理默克尔滑雪时摔倒 骨盆断裂','date'=>'2014-1-6'),
+	array('title'=>'德国女总理默克尔滑雪时摔倒 骨盆断裂','date'=>'2014-1-6'),
+);
+$arr1 = array('user'=>'lily', 'age'=>30);
+echo json_encode($arr);
